c2cb0152450d00512588216cb66a7f7f789f195d
Richard Cooper <Richard.Cooper@bennetts.co.uk>
2016-11-18 10:12:44 +0000

    Add swagger

--- src/ProductApi/Startup.cs
+++ src/ProductApi/Startup.cs
@@ -25,6 +25,7 @@
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@
             loggerFactory.AddDebug();
             app.UseStaticFiles();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi();
         }
     }
 }
